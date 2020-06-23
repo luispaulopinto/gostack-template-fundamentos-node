8f3f9a4b6eec56ac935d02aaa0732e806e256559
Luis Paulo Pinto <lp_paulo85@yahoo.com.br>
2020-06-22 23:36:16 -0300

    Initial commit: Challenge done.

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,60 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  // private balance: Omit<Balance, 'total'>;
+
   constructor() {
     this.transactions = [];
+    // this.balance = { outcome: 0, income: 0 };
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomeReducer2 = (a: number, c: Transaction): number => {
+      return c.type === 'income' ? a + c.value : a + 0;
+    };
+
+    const outcomeReducer = (a: number, c: Transaction): number => {
+      return c.type === 'outcome' ? a + c.value : a + 0;
+    };
+
+    const income = this.transactions.reduce(incomeReducer2, 0);
+    const outcome = this.transactions.reduce(outcomeReducer, 0);
+
+    return {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    // return {
+    //   income: this.balance.income || 0,
+    //   outcome: this.balance.outcome || 0,
+    //   total: this.balance.income - this.balance.outcome || 0,
+    // };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    // if (type === 'income') this.balance.income += value;
+    // else this.balance.outcome += value;
+
+    return transaction;
   }
 }
 
